9cc1be50ba8e5bf72752c66a33d796da0cb20653
Asutosh <asutosh97@users.noreply.github.com>
2016-10-09 22:08:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //  Change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+//  Move the image
+var img = document.getElementById('photo');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}
