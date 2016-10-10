dd5c9669d803f35a27fb5a676155a4a0a1140f62
Asutosh <asutosh97@users.noreply.github.com>
2016-10-11 02:09:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,18 +78,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-app.get('/counter', function (req, res) 
-{
-  counter = counter + 1;
-  res.send(counter.toString());  
-});
-
 var names = [];
 app.get('/search-name/:name',function (req, res) {
    var name = req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
+});
+
+app.get('/counter', function (req, res) 
+{
+  counter = counter + 1;
+  res.send(counter.toString());  
 });
 
 
